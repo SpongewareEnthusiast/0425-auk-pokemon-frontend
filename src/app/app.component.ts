4949fae0d4453c266fe95d158e23e4bcbaf31c76
SpongewareEnthusiast <c999m@protonmail.com>
2025-04-24 23:11:50 +0100

    style + win tracker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RandomPokemonService } from './random-pokemon.service';
 import { RandomPokemon } from './random-pokemon.model';
-import { NgClass, NgIf, TitleCasePipe } from '@angular/common';
+import { NgOptimizedImage, CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, NgClass, TitleCasePipe],
+  imports: [RouterOutlet, CommonModule, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -17,10 +17,20 @@ export class AppComponent {
   loading = false;
   result: any | null = null;
   resultLoading: any | null = null;
+  score: number = 0
+  scoreArray: string[] = [" ⬜️ "," ⬜️" ," ⬜️ "," ⬜️ "," ⬜️ "," ⬜️ "," ⬜️ "," ⬜️ "," ⬜️ "," ⬜️ "];
+  attempts: number = 0;
   constructor(private randomPokemonService: RandomPokemonService) {}
 
   ngOnInit() {
+   this.initialiseScore();
     this.getRandomPokemon()
+  }
+  initialiseScore() {
+    this.score = 0;
+    this.attempts = 0;
+    this.scoreArray= [" ⬜️ "," ⬜️" ," ⬜️ "," ⬜️ "," ⬜️ "," ⬜️ "," ⬜️ "," ⬜️ "," ⬜️ "," ⬜️ "];
+
   }
   getRandomPokemon() {
     this.result = null;
@@ -39,12 +49,25 @@ export class AppComponent {
   }
   checkPokemon(id: number, name: string) {
     this.resultLoading = true;
-    console.log(id, name)
     this.randomPokemonService.checkPokemon(id, name).subscribe({
       next: (data) => {
-        console.log(data)
+        this.attempts++;
         this.result = data;
         this.resultLoading = false;
+        if (this.result.is_correct) {
+          this.score++;
+          this.scoreArray[this.attempts-1]=(" ✅ ")
+        } else {
+          this.scoreArray[this.attempts-1]=(" ❌ ")
+        }
+        if (this.score == 7) {
+          this.initialiseScore();
+          alert('You won! 🥳')
+        }
+        if (this.attempts == 10) {
+          this.initialiseScore();
+          alert("You lost 🥹")
+        }
       },
       error: (err) => {
         console.error("error", err)
