60b3da770d5aaf9bad853defe3507969462ebd6c
SpongewareEnthusiast <c999m@protonmail.com>
2025-04-24 23:21:13 +0100

    readme + typo

--- src/app/random-pokemon.service.ts
+++ src/app/random-pokemon.service.ts
@@ -2,24 +2,29 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface RandomPokemon {
+  name: string,
+  id: number,
+  image: URL,
+  guesses: Array<string>
+}
+export interface ValidatedAnswer {
+  is_correct: boolean;
+  correct_name: string;
+  image: string;
+}
 @Injectable({
   providedIn: 'root'
 })
-// export interface RandomPokemon {
-//   name: string,
-//   id: number,
-//   image: URL,
-//   guesses: Array<string>
-// }
 export class RandomPokemonService {
   private randomPokemonAPIURL = "http://localhost:8000/random-pokemon"
   private checkPokemonAPIURL = "http://localhost:8000/validate-answer"
   constructor(private http: HttpClient) { }
-  getRandomPokemon(): Observable<any> {
-    return this.http.get(this.randomPokemonAPIURL)
+  getRandomPokemon(): Observable<RandomPokemon> {
+    return this.http.get<RandomPokemon>(this.randomPokemonAPIURL)
   }
 
   checkPokemon(id: number, name: string): Observable<any> {
-    return this.http.get(this.checkPokemonAPIURL + `?id=${id}&name=${name}`)
+    return this.http.get<ValidatedAnswer>(this.checkPokemonAPIURL + `?id=${id}&name=${name}`)
   }
 }
